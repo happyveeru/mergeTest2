11c536a519f2a1940cb4998b2321fe0312c78f9d
happyveeru <happyveeru@gmail.com>
2022-09-13 04:17:36 -0400

    Some other change to branch4

--- VeeraGitTest/src/com/veera/test/M2.java
+++ VeeraGitTest/src/com/veera/test/M2.java
@@ -5,6 +5,7 @@ public class M2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Change4 deleted M3 file as well");
+		System.out.println("Change4some other change");
 	}
 
 }
