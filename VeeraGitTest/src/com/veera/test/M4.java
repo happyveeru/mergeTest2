2c199299bb84bffa0105a5b579277436b4fd723e
happyveeru <happyveeru@gmail.com>
2022-09-14 22:09:21 -0400

    Addressed the revew comments

--- VeeraGitTest/src/com/veera/test/M4.java
+++ VeeraGitTest/src/com/veera/test/M4.java
@@ -4,7 +4,8 @@ public class M4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("REsove conversation commit");
+		// System.out.println("REsove conversation commit");
+		// Additional
 	}
 
 }
