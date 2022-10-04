6dd075b30cc203a2315ae9dcf1f57aecc9b62958
happyveeru <37739932+happyveeru@users.noreply.github.com>
2022-10-04 03:09:51 -0400

    Revert "Branch3"

--- VeeraGitTest/src/com/veera/test/M2.java
+++ VeeraGitTest/src/com/veera/test/M2.java
@@ -6,9 +6,8 @@ public class M2 {
 		// TODO Auto-generated method stub
 		System.out.println("Change4 deleted M3 file as well");
 		System.out.println("Change4some other change");
-		//branch 'Branch3' of https://github.com/happyveeru/mergeTest2.git
+		branch 'Branch3' of https://github.com/happyveeru/mergeTest2.git
 		System.out.println("AUt change Branch3");
-		// one more comment
 	}
 
 }
