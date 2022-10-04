8839aff0c5cbd20a9e9a98dfb0aed1fdf3fb0373
happyveeru <happyveeru@gmail.com>
2022-10-04 03:19:27 -0400

    Revert Additional Commits

--- VeeraGitTest/src/com/veera/test/M2.java
+++ VeeraGitTest/src/com/veera/test/M2.java
@@ -4,10 +4,12 @@ public class M2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Change4 deleted M3 file as well");
-		System.out.println("Change4some other change");
-		branch 'Branch3' of https://github.com/happyveeru/mergeTest2.git
+		//System.out.println("Change4 deleted M3 file as well");
+		//System.out.println("Change4some other change");
+		//branch 'Branch3' of https://github.com/happyveeru/mergeTest2.git
 		System.out.println("AUt change Branch3");
+		System.out.println("Revert then changes");
+		System.out.println("Revert then changes2");
 	}
 
 }
